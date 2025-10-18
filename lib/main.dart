e84d830fa2e0748eb03fa892de8407232f9cf066
azref <azreftravaux@gmail.com>
2025-10-18 23:22:25 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+yimport 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
